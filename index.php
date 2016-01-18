5c76fd0c26cf20f5ebe5f4cf5a8d65ed997b2283
Guðni Natan Gunnarsson <gudni.nathan@gmail.com>
2016-01-18 14:55:34 +0000

    Grunnur

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<head>
+    <title>Dub16</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="GRU2L4U Vefsíða">
+    <meta name="author" content="Guðni Natan Gunnarsson, Jóhann Rúnarsson, Óli Pétur Olsen">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="stilsida.css">
+</head>
+<body>
+    <nav class="custom-wrapper pure-g" id="menu">
+            <p>Myndasíða</p>
+    </nav>
+<main>
+    <article>
+        <p>Titill á mynd</p>
+            <figure class="img">
+            <a href="browse.php#nafn"><img src="img/slideshow/1.jpg"></a>
+            </figure>
+        </article>
+    <article>
+        <p>Titill á mynd</p>
+        <figure class="img">
+            <img src="img/slideshow/2.jpg">
+        </figure>
+    </article>
+    <article>
+        <p>Titill á mynd</p>
+        <figure class="img">
+            <img src="img/slideshow/3.jpg">
+        </figure>
+    </article>
+    <article>
+        <p>Titill á mynd</p>
+        <figure class="img">
+            <img src="img/slideshow/4.jpg">
+        </figure>
+    </article>
+    <article>
+        <p>Titill á mynd</p>
+        <figure class="img">
+            <img src="img/slideshow/5.jpg">
+        </figure>
+    </article>
+    <article>
+        <p>Titill á mynd</p>
+        <figure class="img">
+            <img src="img/slideshow/1.jpg">
+        </figure>
+    </article>
+    <article>
+        <p>Titill á mynd</p>
+        <figure class="img">
+            <img src="img/slideshow/1.jpg">
+        </figure>
+    </article>
+    <article>
+        <p>Titill á mynd</p>
+        <figure class="img">
+            <img src="img/slideshow/2.jpg">
+        </figure>
+    </article>
+    <article>
+        <p>Titill á mynd</p>
+        <figure class="img">
+            <img src="img/slideshow/3.jpg">
+        </figure>
+    </article>
+    <article>
+        <p>Titill á mynd</p>
+        <figure class="img">
+            <img src="img/slideshow/4.jpg">
+        </figure>
+    </article>
+    <article>
+        <p>Titill á mynd</p>
+        <figure class="img">
+            <img src="img/slideshow/5.jpg">
+        </figure>
+    </article>
+    <article>
+        <p>Titill á mynd</p>
+        <figure class="img">
+            <img src="img/slideshow/1.jpg">
+        </figure>
+    </article>
+</main>
+<footer>
+    <p>2016, Allur réttur áskilinn</p>
+    <p>Guðni Natan Gunnarsson</p>
+</footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+</body>
+</html>
