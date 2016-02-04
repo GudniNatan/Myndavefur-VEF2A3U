928ed170c8bc0338b918f2283bc4e635042f1fda
Guðni Natan Gunnarsson <gudni.nathan@gmail.com>
2016-02-04 12:10:53 +0000

    Nota núna glob í staðinn fyrir scandir
    Myndum er raðað í tímaröð!

--- includes/images.php
+++ includes/images.php
@@ -1,11 +1,12 @@
 <?php
-    $dir = 'img/showcase';
-    $files = preg_grep('/^([^.])/', scandir($dir));
-    foreach ($files as $key => $value) {
+    $dir = 'img/showcase/';
+    array_multisort(array_map('filemtime', ($images = glob("{$dir}*.{jpg,png,gif,jpeg}", GLOB_BRACE))), SORT_DESC, $images);
+
+    foreach ($images as $key => $value) {
         echo "<article>
-            <p>$value</p>
+            <p>".basename($value)."</p>
             <figure class='img'>
-                <a href='browse.php?img={$key}'><img src='{$dir}/{$value}'></a>
+                <a href='browse.php?img={$key}'><img src='{$value}'></a>
             </figure>
         </article>
         ";
