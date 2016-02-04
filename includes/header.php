8e3632bc766910779922acacaec5299bc1da5e76
Guðni Natan Gunnarsson <gudni.nathan@gmail.com>
2016-02-04 12:17:37 +0000

    Header notar líka glob í staðinn fyrir scandir

--- includes/header.php
+++ includes/header.php
@@ -1,14 +1,14 @@
 <?php
 //Velur random mynd í header
-    $dir = 'img/header';
+    $dir = 'img/header/';
     if (isset($_SESSION["files"]) == false || count($_SESSION["files"]) == 0) {
-    	$_SESSION["files"] = preg_grep('/^([^.])/', scandir($dir));
+    	array_multisort(array_map('filemtime', ($_SESSION["files"] = glob("{$dir}*.{jpg,png,gif,jpeg}", GLOB_BRACE))), SORT_DESC, $_SESSION["files"]);
     }
     shuffle($_SESSION["files"]);
     $imgpath = $_SESSION["files"][0];
     unset($_SESSION["files"][0]);
   ?>
-<header class="custom-wrapper pure-g" id="menu" style="background-image: url('img/header/<?php echo $imgpath; ?>')">
+<header class="custom-wrapper pure-g" id="menu" style="background-image: url('<?php echo $imgpath; ?>')">
 	<div>
 		<a href="index.php" class="headerlink"><h1 class="_i_myndun">(í)<i>myndun</i></h1></a>
 	</div>
